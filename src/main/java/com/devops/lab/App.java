242e62c9af7ad6f661c8375a530a0461bd49de2f
2021wa86442 <2021wa86442@wilp.bits-pilani.ac.in>
2025-07-10 02:59:52 +0530

    Updated App.java Manually

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -1,13 +1,11 @@
 package com.devops.lab;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello from Continuous Integration Trigger!" );
+	System.out.println(" DevOpsLab4 completed on basis of first line of output!");
     }
 }
